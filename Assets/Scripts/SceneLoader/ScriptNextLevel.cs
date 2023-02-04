b4a9b78bc418942504456a03650c9b3e06946052
tawan280645 <106896189+tawan280645@users.noreply.github.com>
2023-02-05 05:28:02 +0700

    LoadScene-Sprite

--- Assets/Scripts/SceneLoader/ScriptNextLevel.cs
+++ Assets/Scripts/SceneLoader/ScriptNextLevel.cs
@@ -7,6 +7,6 @@ public class ScriptNextLevel : MonoBehaviour
 {
     public void NextLevelButton()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }

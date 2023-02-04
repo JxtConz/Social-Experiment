56730603aed53f7d2c4532c6b7362c53fd75af27
Franziskus Heblich <git@heblich.eu>
2023-02-04 15:27:42 +0700

    rebase enemy WIP

---
+++ Assets/Scripts/Enemy/HitPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+
+public interface HitPlayer
+{
+    public enum HitType
+    {
+        RootTip,
+        RootBranch
+    }
+
+    void HitPlayer(GameObject source, int amount, HitType hit);
+}

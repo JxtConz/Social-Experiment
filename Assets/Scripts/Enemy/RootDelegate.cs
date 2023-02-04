5e2fbaf2d5eaa3aab0a60020d73002345bfcb8ad
Franziskus Heblich <git@heblich.eu>
2023-02-04 19:11:13 +0700

    enemy firt version

---
+++ Assets/Scripts/Enemy/RootDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class RootDelegate : MonoBehaviour, HitAbleEnemy
+    {
+        public HitAbleEnemy owner;
+
+        public bool HitEnemy(int damage)
+        {
+            return owner.HitEnemy(damage);
+        }
+    }
+}
